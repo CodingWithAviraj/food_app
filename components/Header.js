9b9b79e2f29392591257ca5c33f6ca594218ab2a
Aviraj <panditaviraj99@gmail.com>
2024-04-28 00:57:58 +0530

    handling files in header

--- components/Header.js
+++ components/Header.js
@@ -2,69 +2,57 @@
 import Image from "next/image";
 import brandLogoPizza from '../resource/brandLogoPizza.png';
 import downArrow from '../resource/downArrow.svg';
+import LoginIcon from "./svg_icons/loginIcon";
+import CartIcon from "./svg_icons/cartIcon";
+import SearchIcon from "./svg_icons/searchIcon";
+import MenuIcon from "./svg_icons/menuIcon";
+import LocationIcon from "./svg_icons/locationIcon";
 
-const Header = function () {
+const header = function () {
   return (
-    <div id="header" className='container mx-auto px-2 flex items-center justify-between'>
-      <div id="left-header" className="flex items-center gap-4 justify-between">
-        <div id="menu" role="img" tabIndex={-1} className="cursor-pointer">
-          <svg className="size-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <mask id="mask0_373_3819" maskUnits="userSpaceOnUse">
-              <rect className="size-6" fill="#D9D9D9"></rect>
-            </mask>
-            <g mask="url(#mask0_373_3819)">
-              <path d="M3 18V16H21V18H3ZM3 13V11H21V13H3ZM3 8V6H21V8H3Z" fill="Black"></path>
-            </g>
-          </svg>
-        </div>
-        <div id="brand-logo" className="cursor-pointer">
-          <Image quality={60} priority={true} src={brandLogoPizza} alt="Brand Logo" className="h-12 w-12">
-          </Image>
-        </div>
-        <div id="delivery-location">
-          <div className="flex items-center flex-col h-20 justify-center cursor-pointer">
-            <div className="flex items-center gap-1">
-              <span role="img">
-                <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5.00004 6.00033C5.32087 6.00033 5.59562 5.88599 5.82429 5.65733C6.05257 5.42905 6.16671 5.15449 6.16671 4.83366C6.16671 4.51283 6.05257 4.23808 5.82429 4.00941C5.59562 3.78113 5.32087 3.66699 5.00004 3.66699C4.67921 3.66699 4.40465 3.78113 4.17637 4.00941C3.94771 4.23808 3.83337 4.51283 3.83337 4.83366C3.83337 5.15449 3.94771 5.42905 4.17637 5.65733C4.40465 5.88599 4.67921 6.00033 5.00004 6.00033ZM5.00004 10.2878C6.18615 9.19894 7.06601 8.2096 7.63962 7.31983C8.21323 6.43044 8.50004 5.6406 8.50004 4.95033C8.50004 3.8906 8.1621 3.0228 7.48621 2.34691C6.81071 1.67141 5.98199 1.33366 5.00004 1.33366C4.0181 1.33366 3.18918 1.67141 2.51329 2.34691C1.83779 3.0228 1.50004 3.8906 1.50004 4.95033C1.50004 5.6406 1.78685 6.43044 2.36046 7.31983C2.93407 8.2096 3.81393 9.19894 5.00004 10.2878ZM5.00004 11.8337C3.43476 10.5017 2.26576 9.26447 1.49304 8.12191C0.71993 6.97974 0.333374 5.92255 0.333374 4.95033C0.333374 3.49199 0.802569 2.33019 1.74096 1.46491C2.67896 0.599631 3.76532 0.166992 5.00004 0.166992C6.23476 0.166992 7.32112 0.599631 8.25912 1.46491C9.19751 2.33019 9.66671 3.49199 9.66671 4.95033C9.66671 5.92255 9.28035 6.97974 8.50762 8.12191C7.73451 9.26447 6.56532 10.5017 5.00004 11.8337Z" fill="#151515"></path>
-                </svg>
-              </span>
-              <p className="font-light text-xs">Delivering to</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <p className="font-medium text-sm max-lg:text-ellipsis">Change Address</p>
-              <Image src={downArrow} alt="arrow" className="h-4 w-4">
-              </Image>
+    <div id="header" className="shadow-md">
+      <div className='container mx-auto px-2 flex items-center justify-between'>
+        <div id="left-header" className="flex items-center gap-4 justify-between">
+          <div id="menu" role="img" tabIndex={-1} className="cursor-pointer">
+            <MenuIcon></MenuIcon>
+          </div>
+          <div id="brand-logo" className="cursor-pointer">
+            <Image quality={60} priority={true} src={brandLogoPizza} alt="Brand Logo" className="h-12 w-12">
+            </Image>
+          </div>
+          <div id="delivery-location">
+            <div className="flex items-center flex-col h-20 justify-center cursor-pointer">
+              <div className="flex items-center gap-1">
+                <LocationIcon></LocationIcon>
+                <p className="font-light text-xs">Delivering to</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="font-medium text-sm max-lg:text-ellipsis">Change Address</p>
+                <Image src={downArrow} alt="arrow" className="h-4 w-4">
+                </Image>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div id="right-header" className="flex items-center gap-4 justify-between">
-        <div id="search" className="flex items-center gap-1 cursor-pointer">
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.9999 14L11.0093 11.004M12.6666 7.00004C12.6666 8.50293 12.0696 9.94427 11.0069 11.007C9.94415 12.0697 8.50281 12.6667 6.99992 12.6667C5.49703 12.6667 4.05569 12.0697 2.99298 11.007C1.93027 9.94427 1.33325 8.50293 1.33325 7.00004C1.33325 5.49715 1.93027 4.05581 2.99298 2.9931C4.05569 1.9304 5.49703 1.33337 6.99992 1.33337C8.50281 1.33337 9.94415 1.9304 11.0069 2.9931C12.0696 4.05581 12.6666 5.49715 12.6666 7.00004V7.00004Z" stroke="#151515" strokeWidth="1.8" strokeLinecap="round"></path>
-          </svg>
-          <p className="font-medium text-base max-md::text-ellipsis">
-            Search
-          </p>
-        </div>
-        <div id="login" className="flex items-center gap-1 h-10 cursor-pointer">
-          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11.5 6.75C11.5 7.41304 11.2366 8.04893 10.7678 8.51777C10.2989 8.98661 9.66304 9.25 9 9.25C8.33696 9.25 7.70107 8.98661 7.23223 8.51777C6.76339 8.04893 6.5 7.41304 6.5 6.75C6.5 6.08696 6.76339 5.45107 7.23223 4.98223C7.70107 4.51339 8.33696 4.25 9 4.25C9.66304 4.25 10.2989 4.51339 10.7678 4.98223C11.2366 5.45107 11.5 6.08696 11.5 6.75ZM10.4142 8.16421C10.7893 7.78914 11 7.28043 11 6.75C11 6.21957 10.7893 5.71086 10.4142 5.33579C10.0391 4.96071 9.53043 4.75 9 4.75C8.46957 4.75 7.96086 4.96071 7.58579 5.33579C7.21071 5.71086 7 6.21957 7 6.75C7 7.28043 7.21071 7.78914 7.58579 8.16421C7.96086 8.53929 8.46957 8.75 9 8.75C9.53043 8.75 10.0391 8.53929 10.4142 8.16421Z" fill="#151515" stroke="#151515"></path><path fillRule="evenodd" clipRule="evenodd" d="M9 0.75C4.44375 0.75 0.75 4.44375 0.75 9C0.75 13.5562 4.44375 17.25 9 17.25C13.5562 17.25 17.25 13.5562 17.25 9C17.25 4.44375 13.5562 0.75 9 0.75ZM2.25 9C2.25 10.5675 2.78475 12.0105 3.681 13.1565C4.31042 12.3299 5.12243 11.6601 6.05357 11.1992C6.98472 10.7384 8.00981 10.4991 9.04875 10.5C10.0742 10.499 11.0864 10.7321 12.0082 11.1816C12.93 11.631 13.737 12.2849 14.3678 13.0935C15.0176 12.2412 15.4551 11.2464 15.6442 10.1914C15.8332 9.13647 15.7683 8.05165 15.4549 7.02674C15.1414 6.00183 14.5884 5.06629 13.8416 4.29753C13.0948 3.52877 12.1757 2.94889 11.1603 2.60588C10.1449 2.26287 9.0624 2.16658 8.00239 2.32498C6.94239 2.48338 5.93534 2.89192 5.06458 3.51679C4.19381 4.14166 3.48436 4.9649 2.99492 5.9184C2.50549 6.87189 2.25014 7.92823 2.25 9ZM9 15.75C7.45046 15.7523 5.9477 15.2193 4.746 14.241C5.2297 13.5486 5.8735 12.9832 6.62264 12.593C7.37179 12.2028 8.20409 11.9994 9.04875 12C9.88287 11.9993 10.7051 12.1977 11.4472 12.5786C12.1893 12.9595 12.8298 13.5119 13.3155 14.19C12.1045 15.2 10.5769 15.7522 9 15.75Z" fill="#151515"></path>
-          </svg>
-          <p className="font-medium text-base max-md:text-ellipsis"> Login </p>
-        </div>
-        <div id="cart" className="flex items-center gap-1 cursor-pointer">
-          <span role="img">
-            <svg width="17" height="15" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M15.8971 10.3029H5.4293L5.95488 9.23242L14.6877 9.2166C14.983 9.2166 15.2361 9.00566 15.2889 8.71387L16.4982 1.94453C16.5299 1.76699 16.4824 1.58418 16.3664 1.44531C16.3091 1.37697 16.2375 1.32192 16.1568 1.28398C16.0761 1.24603 15.988 1.22611 15.8988 1.22559L4.78945 1.18867L4.69453 0.742188C4.63477 0.457422 4.37812 0.25 4.08633 0.25H1.37051C1.20594 0.25 1.04811 0.315375 0.931743 0.431742C0.815375 0.54811 0.75 0.705939 0.75 0.870508C0.75 1.03508 0.815375 1.19291 0.931743 1.30927C1.04811 1.42564 1.20594 1.49102 1.37051 1.49102H3.58359L3.99844 3.46328L5.01973 8.40801L3.70488 10.5543C3.6366 10.6465 3.59547 10.7559 3.58615 10.8702C3.57683 10.9845 3.5997 11.0992 3.65215 11.2012C3.75762 11.4104 3.97031 11.5422 4.20586 11.5422H5.30977C5.07443 11.8548 4.94731 12.2355 4.94766 12.6268C4.94766 13.6217 5.75625 14.4303 6.75117 14.4303C7.74609 14.4303 8.55469 13.6217 8.55469 12.6268C8.55469 12.2348 8.42461 11.8533 8.19258 11.5422H11.0244C10.7891 11.8548 10.662 12.2355 10.6623 12.6268C10.6623 13.6217 11.4709 14.4303 12.4658 14.4303C13.4607 14.4303 14.2693 13.6217 14.2693 12.6268C14.2693 12.2348 14.1393 11.8533 13.9072 11.5422H15.8988C16.2398 11.5422 16.5193 11.2645 16.5193 10.9217C16.5183 10.7573 16.4523 10.6 16.3357 10.484C16.2192 10.3681 16.0615 10.303 15.8971 10.3029ZM5.04785 2.41211L15.1693 2.44551L14.1779 7.99668L6.22734 8.01074L5.04785 2.41211ZM6.75117 13.1822C6.44531 13.1822 6.1957 12.9326 6.1957 12.6268C6.1957 12.3209 6.44531 12.0713 6.75117 12.0713C7.05703 12.0713 7.30664 12.3209 7.30664 12.6268C7.30664 12.7741 7.24812 12.9154 7.14395 13.0195C7.03978 13.1237 6.89849 13.1822 6.75117 13.1822ZM12.4658 13.1822C12.16 13.1822 11.9104 12.9326 11.9104 12.6268C11.9104 12.3209 12.16 12.0713 12.4658 12.0713C12.7717 12.0713 13.0213 12.3209 13.0213 12.6268C13.0213 12.7741 12.9628 12.9154 12.8586 13.0195C12.7544 13.1237 12.6131 13.1822 12.4658 13.1822Z" fill="#151515" stroke="#151515" strokeWidth="0.3"></path>
-            </svg>
-          </span>
-          <p className="font-medium text-base max-md:text-ellipsis"> Cart </p>
+        <div id="right-header" className="flex items-center gap-4 justify-between">
+          <div id="search" className="flex items-center gap-1 cursor-pointer">
+            <SearchIcon></SearchIcon>
+            <p className="font-medium text-base max-md::text-ellipsis">
+              Search
+            </p>
+          </div>
+          <div id="login" className="flex items-center gap-1 h-10 cursor-pointer">
+            <LoginIcon></LoginIcon>
+            <p className="font-medium text-base max-md:text-ellipsis"> Login </p>
+          </div>
+          <div id="cart" className="flex items-center gap-1 cursor-pointer">
+            <CartIcon></CartIcon>
+            <p className="font-medium text-base max-md:text-ellipsis"> Cart </p>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default Header;
+export default header;
---
+++ components/svg_icons/menuIcon.js
@@ -0,0 +1,14 @@
+
+const menuIcon = function () {
+    return (
+        <svg className="size-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_373_3819" maskUnits="userSpaceOnUse">
+                <rect className="size-6" fill="#D9D9D9"></rect>
+            </mask>
+            <g mask="url(#mask0_373_3819)">
+                <path d="M3 18V16H21V18H3ZM3 13V11H21V13H3ZM3 8V6H21V8H3Z" fill="Black"></path>
+            </g>
+        </svg>
+    );
+}
+export default menuIcon;
