f7191f9ddcaf10d62491717142c0765b482f56c8
Ravi Singh <innocentravi40@gmail.com>
2024-05-11 15:21:49 +0530

    imageswiper added

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
+"use client"
 import React from "react";
 import Header from "@/components/Header";
 import Card from "@/components/Card";
 // import MenuCategories from "@/components/MenuCategories";
 // import CartSummary from "@/components/CartSummary";
 import MenuPanel from "@/components/MenuPanel";
-
+import ImageSwiper from "@/components/ImageSwiper";
+// import { Virtuoso } from 'react-virtuoso'
 const app = function () {
   return (
     <div className="bg-white">
       <Header />
+      <ImageSwiper />
       <Card />
       <MenuPanel />
       <div>
