5ab8067cad1202a7c31c6ae0f7ac8e4245584bf9
Aviraj <panditaviraj99@gmail.com>
2024-05-14 22:26:21 +0530

    adding carousel.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,15 @@
 "use client"
 import React from "react";
 import Header from "@/components/Header";
-import Card from "@/components/Card";
-// import MenuCategories from "@/components/MenuCategories";
-// import CartSummary from "@/components/CartSummary";
 import MenuPanel from "@/components/MenuPanel";
-import ImageSwiper from "@/components/ImageSwiper";
-// import { Virtuoso } from 'react-virtuoso'
+import CustomCarousel from "@/components/common/CustomCarousel";
+
 const app = function () {
   return (
     <div className="bg-white">
       <Header />
       <div className="w-full m-auto max-w-[900px] ">
-          <ImageSwiper />
+          <CustomCarousel />
           <MenuPanel />
       </div>
       <div className="bg-blue-200">
